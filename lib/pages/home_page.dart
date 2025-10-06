22270b736180c59ff772dcf338a3f116b35de7f1
Rudra <rudragoswami4331@gmail.com>
2025-10-07 00:58:42 +0530

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,6 @@
 //Packages
 import 'package:flutter/material.dart';
 
-//Pages
-import '../pages/chats_page.dart';
-import '../pages/users_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,11 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentPage = 0;
-  final List<Widget> _pages = [
-    ChatsPage(),
-    UsersPage(),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,30 +17,6 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      body: _pages[_currentPage],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentPage,
-        onTap: (_index) {
-          setState(() {
-            _currentPage = _index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            label: "Chats",
-            icon: Icon(
-              Icons.chat_bubble_sharp,
-            ),
-          ),
-          BottomNavigationBarItem(
-            label: "Users",
-            icon: Icon(
-              Icons.supervised_user_circle_sharp,
-            ),
-          ),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
